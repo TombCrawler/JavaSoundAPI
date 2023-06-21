c8386d51746e52fc417181a5eaf02d525259ddd0
YA <ya@YAs-MacBook-Pro.local>
2023-06-21 21:51:42 +0900

    add DJ feature

--- Main1.java
+++ Main1.java
@@ -1,5 +1,4 @@
 import javax.sound.sampled.*;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,9 +6,16 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
+// for DJ
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
 public class Main1 {
 
     private static Clip clip;
+    private static float volume = 1.0f; // Declare variable as Default volume
+    private static float balance = 0.0f; // Default balance
 
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
@@ -43,10 +49,10 @@ public class Main1 {
         // Load background image
         ImageIcon imageIcon = new ImageIcon("boombox.png");
         Image image = imageIcon.getImage();
-        Image scaledImage = image.getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+        Image scaledImage = image.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
         ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
         JLabel backgroundLabel = new JLabel(scaledImageIcon);
-        backgroundLabel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+        backgroundLabel.setBounds(0, -90, frame.getWidth(), frame.getHeight());
         frame.getContentPane().add(backgroundLabel, 0); //add as the bottom layer
 
 
@@ -81,15 +87,58 @@ public class Main1 {
             }
         });
 
+        // volume slider
+        JSlider volumeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, (int) (volume * 100));
+        volumeSlider.setBounds(50, 280, 300, 30);
+        volumeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                if (!source.getValueIsAdjusting()) {
+                    int value = source.getValue();
+                    volume = value / 100.0f;
+                    adjustVolume();
+                }
+            }
+        });
+        // Create a label for the volume slider
+        JLabel volumeLabel = new JLabel("Volume");
+        volumeLabel.setBounds(50, 270, 100, 20);
+
+        // Balance slider
+        JSlider balanceSlider = new JSlider(JSlider.HORIZONTAL, -100, 100, (int) (balance * 100));
+        balanceSlider.setBounds(50, 230, 300, 30);
+        balanceSlider.addChangeListener(new ChangeListener() {
+        @Override
+        public void stateChanged(ChangeEvent e) {
+                JSlider source = (JSlider) e.getSource();
+                if (!source.getValueIsAdjusting()) {
+                    int value = source.getValue();
+                    balance = value / 100.0f;
+                    adjustBalance();
+                }
+            }
+        });
+        // Create a label for the balance slider
+        JLabel balanceLabel = new JLabel("Pan");
+        balanceLabel.setBounds(50, 220, 100, 20);
+
+
         frame.add(playButton);
         frame.add(stopButton);
         frame.add(resetButton);
+        frame.add(volumeSlider);
+        frame.add(volumeLabel);
+        frame.add(balanceSlider);
+        frame.add(balanceLabel);
 
         frame.setVisible(true);
     }
 
     private static void play() {
         if (!clip.isRunning()) {
+            adjustVolume();
+            adjustBalance();
             clip.start();
         }
     }
@@ -102,5 +151,20 @@ public class Main1 {
 
     private static void reset() {
         clip.setMicrosecondPosition(0);
+    }
+
+private static void adjustVolume() {
+        if (clip != null) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
+            gainControl.setValue(dB);
+        }
+    }
+
+private static void adjustBalance() {
+        if (clip != null) {
+            FloatControl balanceControl = (FloatControl) clip.getControl(FloatControl.Type.BALANCE);
+            balanceControl.setValue(balance);
+        }
     }
 }
